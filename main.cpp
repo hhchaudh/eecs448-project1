62bdeafd81fd73b4ddba5e5cf77da8b29bbeb987
Brad Gibbons <b180g073@ku.edu>
2016-09-09 17:38:47 -0500

    Modified main.cpp to have a simple decleration and includes. Removed Makefile.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /**
 *	@file : main.cpp
-*	@author: Brad Gibbons
+*	@author: Brad Gibbons, Rony Singh, Jackson Schilmoeller, Chris Porras
 *	@date: 2016.09.09
 *	Purpose: Implementation of the program
 */
@@ -10,9 +10,5 @@
 #include "Node.h"
 
 int main() {
-	Node* test = new Node();
-	test->setYear(2016);
-	std::cout << test->getYear() << std::endl;
-	delete test;
 	return(0);
 }
