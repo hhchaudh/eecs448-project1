f6263c3324eb5b063f4722f38196d7d344b1e545
Brad Gibbons <b180g073@ku.edu>
2016-09-09 21:21:41 -0500

    Implemented the pushBack and removeBack methods of the DoubleLinkedList class.

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -62,6 +62,45 @@ void DoubleLinkedList::removeFront() {
 	m_size--; //Decriment size
 }
 
+void DoubleLinkedList::pushBack(int year, int month, int day) {
+	if (m_size == 0) { //If no Nodes have been added
+		Node* temp = new Node();
+		//Set values for the new Node
+		temp->setYear(year);
+		temp->setMonth(month);
+		temp->setDay(day);
+		//Point m_front and m_back
+		m_front = temp;
+		m_back = m_front;
+	} else { //If one or more Nodes have been added already
+		Node* temp = new Node();
+		//Set values for the new Node
+		temp->setYear(year);
+		temp->setMonth(month);
+		temp->setDay(day);
+		//Point the new Node and previous m_back to each other
+		temp->setPrev(m_back);
+		m_back->setNext(temp);
+		//Set m_back as the new Node
+		m_back = temp;
+	}
+	m_size++; //Increment size
+}
+
+void DoubleLinkedList::removeBack() {
+	if (m_size == 1) { //If only one Node in the DoubleLinkedList
+		delete m_front;
+		m_front = nullptr;
+		m_back = nullptr;
+	} else { //If there is more than one Node
+		Node* temp = m_back->getPrev(); //Get the second to last Node in the list
+		temp->setNext(nullptr); //Point that Node to nullptr
+		delete m_back; //Delete the back Node
+		m_back = temp; //Point m_back to the second to last Node
+	}
+	m_size--; //Decriment size
+}
+
 Node* DoubleLinkedList::getFront() const {
 	return(m_front);
 }
