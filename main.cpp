abfd3ece873ee8b66b629878a74dc387e86dc079
Rony Singh <rsingh.lx@gmail.com>
2016-09-15 16:20:37 -0500

    PseudoCode of Main
    Added what's to be done as a beginning outline.

---
+++ main.cpp
@@ -0,0 +1,40 @@
+//Main file that controls the program, calls others methods and creates object files
+//Project One
+//Outline Version
+int main()
+{
+//Create a double linked list object.
+//Create a read/write object.
+//Call read/write object method that populates the double linked list.
+//Create a while loop for when the program is running.
+//Check for user exiting and save the data.
+//Delete linked list.
+//Delete read write.	
+}
+//Method to be called when program is exiting.
+saveData()
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
