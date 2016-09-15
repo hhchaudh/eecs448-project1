0c3cce25668f2de9c33e13a5f71cb841326634fa
Rony Singh <rsingh.lx@gmail.com>
2016-09-15 16:36:39 -0500

    Fixed error
    Commit error fixed

--- main.cpp
+++ main.cpp
@@ -1,52 +1,9 @@
-<<<<<<< HEAD
-//Main file that controls the program, calls others methods and creates object files
-//Project One
-//Outline Version
-int main()
-{
-//Create a double linked list object.
-//Create a read/write object.
-//Call read/write object method that populates the double linked list.
-//Create a while loop for when the program is running.
-//Check for user exiting and save the data.
-//Delete linked list.
-//Delete read write.	
-}
-//Method to be called when program is exiting.
-saveData()
-{
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-=======
 /**
 *	@file : main.cpp
 *	@author: Brad Gibbons, Rony Singh, Jackson Schilmoeller, Chris Porras
 *	@date: 2016.09.09
 *	Purpose: Implementation of the program
 */
-
 #include <string>
 #include <iostream>
 #include "Node.h"
@@ -55,4 +12,4 @@ saveData()
 int main() {
 	return(0);
 }
->>>>>>> origin/master
+
