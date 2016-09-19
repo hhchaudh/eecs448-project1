ae940ba45207ccad1e79eee5ae036d121a40b69e
Rony Singh <rsingh.lx@gmail.com>
2016-09-18 20:41:15 -0500

    More updates

--- main.cpp
+++ main.cpp
@@ -21,19 +21,19 @@ int main()
 	writer.getFileInfo(calender);
 	currentDate=writer.getDate();
 	//Program interacting with user now
-	
+	int command = 0;
 	//While loop will go here--------------------------------------------------------
 	while(isRunning)
 	{	
-		int command = 0;
+		command = 0;
 		int y=currentDate[0];
 		int m=currentDate[1];
 		int d=currentDate[2];
 		std::cout << "Current date is: " << y << "," << m << "," << d << std::endl;
 		std::cout << "Enter Command " <<std::endl;
 		std::cout << "1) Go to day" << std::endl;
-		std::cout << "2) Add detatil" << std::endl;
-		std::cout << "3) Remove detial" << std::endl;
+		std::cout << "2) Add detail" << std::endl;
+		std::cout << "3) Remove detail" << std::endl;
 		std::cout << "4) View Details" << std::endl;
 		std::cout << "5) Exit" << std::endl;
 		std::cin >> command;
@@ -42,7 +42,7 @@ int main()
 			int year = 0;
 			int month = 0;
 			int day = 0;
-			std::cout << "Choose a year (2015/2016)" << std::endl;
+			std::cout << "Choose a year (2016/2017)" << std::endl;
 			std::cin >> year;
 			std::cout << "Choose a month (Aug-May)" << std::endl;
 			std::cout << "8)August" << std::endl;
@@ -58,14 +58,17 @@ int main()
 			std::cin >> month;
 			std::cout << "Choose a day (correctly)" << std::endl;
 			std::cin >> day;
+			if(year==2016||year==2017
+			{
 			currentDate[0]=year;
+			}
 			if(month!=6 && month!=7)
 			{
 				currentDate[1]=month;
 			}
 			currentDate[2]=day;	
 		}
-		if(command==2)
+		else if(command==2)
 		{	
 			std::string detail = "";
 			std::cout << "Detail to add:" << std::endl;
@@ -73,7 +76,7 @@ int main()
 			Node* temp = calender->getNode(currentDate[0],currentDate[1],currentDate[2]);
 			temp->addDetail(detail);	
 		}
-		if(command==3)
+		else if(command==3)
 		{	
 			int index = 0;
 			Node* temp = calender->getNode(currentDate[0],currentDate[1],currentDate[2]);
@@ -88,7 +91,7 @@ int main()
 			//Remove detail
 			temp->removeDetail(index);
 		}
-		if(command==4)
+		else if(command==4)
 		{	
 			std::cout << "Details for today" << std::endl;
 			Node* temp = calender->getNode(currentDate[0],currentDate[1],currentDate[2]);
@@ -98,9 +101,13 @@ int main()
 				std::cout << i+") "+ details[i] << std::endl;
 			} 
 		}
-		if(command==5)
+		else if(command==5)
 		{
 			isRunning=false;
+		}
+		else
+		{
+			std::cout << "Wrong Input" << std::endl;
 		}
 	}
 	//Check for user exiting and save the data.
