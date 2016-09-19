d01dd75cc4ac4e2eac25c0bbdc891de411a805c6
Rony Singh <rsingh.lx@gmail.com>
2016-09-18 19:43:33 -0500

    Should be a fully functional program for day view
    Add and removes details,
    shows details.
    picks new day

--- main.cpp
+++ main.cpp
@@ -23,68 +23,74 @@ int main()
 	//Program interacting with user now
 	
 	//While loop will go here--------------------------------------------------------
+	while(isRunning)
+	{	
+		int command = 0;
+		std::cout << "Current date is: "+currentDate[0]+","+currentDate[1]+","+currentDate[2] << std:endl;
+		std::cout << "Enter Command " <<std::endl;
+		std::cout << "1) Go to day" << std::endl;
+		std::cout << "2) Add detatil" << std::endl;
+		std::cout << "3) Remove detial" << std::endl;
+		std::cout << "4) View Details" << std::endl;
+		std::cout << "5) Exit" << std::endl;
+		std::cin >> command;
+		if(command==1)
+		{	
+			int year = 0;
+			int month = 0;
+			int day = 0;
+			std::cout << "Choose a year (2015/2016)" << std::endl;
+			std::cin >> year;
+			std::cout << "Choose a month (Aug-May)" << std::endl;
+			std::cin >> month;
+			std::cout << "Choose a day (correctly)" + calender.numDays(year,month) << std::endl;
+			std::cin >> day;
+			currentDate[0]=year;
+			currentDate[1]=month;
+			currentDate[2]=day;	
+		}
+		if(command==2)
+		{	
+			string detail = "";
+			std::cout << "Detail to add:" << std::endl;
+			std::cin >> detail;
+			Node* temp = calender->getNode(currentDate[0],currentDate[1],currentDate[2]);
+			temp->addDetail(detail);	
+		}
+		if(command==3)
+		{	
+			int index = 0;
+			Node* temp = calender->getNode(currentDate[0],currentDate[1],currentDate[2])
+			std::vector<std::string> details = temp.getDetails();
+			std::cout << "Which detail?" << std::endl;
+			for(int i=0; i<details.length,i++)
+			{
+				std::cout << i+") "+ details[i] << std::endl;
+			} 
+			//Shows details for the day
+			std::cin >> index;
+			//Remove detail
+			temp->removeDetail(index);
+		}
+		if(command==4)
+		{	
+			std::cout << "Details for today" << std::endl;
+			Node* temp = calender->getNode(currentDate[0],currentDate[1],currentDate[2])
+			std::vector<std::string> details = temp.getDetails();
+			for(int i=0; i<details.length,i++)
+			{
+				std::cout << i+") "+ details[i] << std::endl;
+			} 
+		}
+		if(command==5)
+		{
+			isRunning=false;
+		}
+	}
 	//Check for user exiting and save the data.
-//writer.setDate(currentDate);
-//writer.storeFileInfo(calender);
+	//writer.setDate(currentDate);
+	//writer.storeFileInfo(calender);
 	//Delete linked list.
-	delete calender;
-
-
-	
+	delete calender;	
 }
-while(isRunning)
-{	
-	int command = 0;
-	std::cout << "Current date is: "+currentDate[1]+","+currentDate[2]+","+currentDate[3] << std:endl;
-	std::cout << "Enter Command " <<std::endl;
-	std::cout << "1) Go to day" << std::endl;
-	std::cout << "2) Add detatil" << std::endl;
-	std::cout << "3) Remove detial" << std::endl;
-	std::cout << "4) View Details" << std::endl;
-	std::cout << "5) Exit" << std::endl;
-	std::cin >> command;
-	if(command==1)
-	{	
-		int year = 0;
-		int month = 0;
-		int day = 0;
-		std::cout << "Choose a year (2015/2016)" << std::endl;
-		std::cin >> year;
-		std::cout << "Choose a month (Aug-May)" << std::endl;
-		std::cin >> month;
-		std::cout << "Choose a day (correctly)" + calender.numDays(year,month) << std::endl;
-		std::cin >> day;
-		calender.getNode(year,month,day)		
-	}
-	if(command==2)
-	{	
-		string detail = "";
-		std::cout << "Detail to add:" << std::endl;
-		std::cin >> year;
-		
-	}
-	if(command==2)
-	{	
-		string detail = "";
-		std::cout << "Detail to add:" << std::endl;
-		std::cin >> detail;
-		//Add detail	
-	}
-	if(command==3)
-	{	
-		int index = 0;
-		std::cout << "Which detail?" << std::endl;
-		//Shows details for the day
-		std::cin >> index;
-		//Remove detail
-	}
-	if(command==4)
-	{	
-		std::cout << "Details for today" << std::endl;
-		//Shows details	
-	}
-	if(command==5)
-	{
-		isRunning=false;
-	}
-}
+
